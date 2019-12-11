9c1280aad18e2dc06fceea7caa5388df610afd84
Lakitha Dias <lakitha.dias@gmail.com>
2019-12-11 16:54:38 +0530

    App.js

--- 06_react_dicer_roller_app/App.js
+++ 06_react_dicer_roller_app/App.js
@@ -3,13 +3,67 @@ import { StyleSheet, Text, View , Image, TouchableOpacity, Alert} from 'react-na
 
 export default class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      uri: require("./src/images/dice1.png")
+    }
+  }
+
+  getRandomValue = () => {
+   return Math.floor(Math.random() * 6) + 1 ;
+  }
+
   playButtonPressed = () => {
-      Alert.alert("Hello");
+    var rNumber = this.getRandomValue();
+
+    switch(rNumber){
+      case 1:
+        this.setState({
+          uri: require("./src/images/dice1.png")
+        })
+        break;
+
+      case 2:
+        this.setState({
+          uri: require("./src/images/dice1.png")
+        })
+        break;
+
+
+      case 3:
+        this.setState({
+          uri: require("./src/images/dice2.png")
+        })
+        break;
+
+
+      case 4:
+        this.setState({
+          uri: require("./src/images/dice3.png")
+        })
+        break;
+
+      case 5:
+        this.setState({
+          uri: require("./src/images/dice4.png")
+        })
+        break;
+
+      case 6:
+        this.setState({
+          uri: require("./src/images/dice5.png")
+        })
+        break;
+
+      default:
+        break;
+    }
   }
   render() {
     return (
         <View style={styles.container}>
-          <Image source={require("./src/images/dice1.png")}/>
+          <Image source={this.state.uri}/>
             <TouchableOpacity onPress={this.playButtonPressed}>
             <Text style={styles.gamebutton}>Play Game</Text>
           </TouchableOpacity>
