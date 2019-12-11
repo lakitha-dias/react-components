44587cd53da58f01ed83f7dd05ab0c56dc395cba
Lakitha Dias <lakitha.dias@gmail.com>
2019-12-11 10:16:38 +0530

    React constructor  and state

--- 03_react_stateintro/App.js
+++ 03_react_stateintro/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+        text: '',
+        isLoggedIn: false
+    }
+  }
+
+  render() {
+    return(
+        <View>
+          <Text>This is render function</Text>
+        </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
