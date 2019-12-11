d559ec7457dd869a0a5ccb012a3bdb09471a057f
Lakitha Dias <lakitha.dias@gmail.com>
2019-12-11 11:50:17 +0530

    App.js TouchableOpacity

---
+++ 04_react_touchable_app/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View , Button, TouchableOpacity, TouchableHighlight} from 'react-native';
+import {Alert} from "react-native-web";
+
+export default class App extends React.Component{
+
+  myButtonPressed() {
+    Alert.alert("Logout");
+  }
+  render() {
+    return (
+        <View style={styles.container}>
+          <TouchableOpacity onPress={this.myButtonPressed()}>
+            <Text style={styles.text}>Login</Text>
+          </TouchableOpacity>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: "#BB2CD9",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: "#FFFFFF",
+    borderRadius: 10
+  }
+});
