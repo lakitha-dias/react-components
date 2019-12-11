c653e99279e75ae0d59574b990f01361200fe6a9
Lakitha Dias <lakitha.dias@gmail.com>
2019-12-11 10:36:09 +0530

    App.js inputs

--- 03_react_stateintro/App.js
+++ 03_react_stateintro/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+// import components
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component{
 
   constructor(props) {
     super(props);
     this.state = {
-        text: '',
-        isLoggedIn: false
+        text: ""
     }
   }
 
   render() {
     return(
-        <View>
-          <Text>This is render function</Text>
+        <View style={"styles.container"}>
+          <TextInput
+              style={styles.input}
+              placeholderTextColor={"#c1c1c1"}
+              placeholder={"Enter value here.."}
+              onChangeText={
+                (text) => {
+                  this.setState({
+                    text:text
+                  })
+                }
+              }
+          />
         </View>
     );
   }
@@ -23,7 +35,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
+  input: {
+    marginTop: 40,
+    height: 50,
+    width: 200,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 5,
+    borderColor: "#23abab"
+  }
 });
